5cb71333c8ffc06d9cd6aea37605fee4496fdfd1
Olalekan Efunkunle <efunkunleolalekan@gmail.com>
2020-10-04 09:27:17 +0100

    mobile responsiveness?

---
+++ build/precache-manifest.4ae1639af3199ae34444b7d75b1c9d3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5872e7de62e0a62f6c1cd87d2c4e0ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d60c2fb6be4883571cc4",
+    "url": "/static/css/main.6b2c777d.chunk.css"
+  },
+  {
+    "revision": "8961004a6b589eb17a4c",
+    "url": "/static/js/2.e1a0f1bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e1a0f1bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d60c2fb6be4883571cc4",
+    "url": "/static/js/main.3701c6cf.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
